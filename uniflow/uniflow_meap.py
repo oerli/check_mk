04affb77515ee590becef92ac94608282c54d4ed
Roland Mueller <rmu@zuehlke.com>
2019-07-13 09:19:42 +0200

    external list of mfp names, check if card reader is ok

--- uniflow/uniflow_meap.py
+++ uniflow/uniflow_meap.py
@@ -8,6 +8,7 @@ import sys
 import argparse
 import requests
 import time
+import json
 from requests_ntlm import HttpNtlmAuth
 from lxml import etree
 
@@ -17,10 +18,13 @@ class Printer:
         self.deviceType = ""
         self.ipAddress = ""
         self.deviceId = ""
+        self.deviceStatus = "Ok"
         self.timeStatus = "Unknown"
         self.connectionStatus = "Unknown"
+        self.connectionDescription = ""
         self.time = 0
-
+        self.critTime = 28800
+        self.warnTime = 1800
 
 def get_data(connect="", user="", password="", limit=""):
     import requests
@@ -60,18 +64,23 @@ def get_data(connect="", user="", password="", limit=""):
                 printer.deviceType = element.text.replace(" ", "_")
             elif element.attrib['name'] == 'DeviceID':
                 printer.deviceId = element.text
+                if element.text == "MiCard - EEPROM error FW Update necessary!":
+                    printer.deviceStatus = "Warning"
             elif element.attrib['name'] == 'IPAddress':
                 printer.ipAddress = element.text
             elif element.attrib['name'] == "Behaviour":
+                printer.connectionDescription = element.text
                 if element.text == "MOMLM=connected=LMCARDTYPELOGIN=Card Type Login@MOMCLIENT=connected=SECUREPRINT=Secure Print Behavior":
                     printer.connectionStatus = "Ok"
+                elif element.text == "MOMLM=connected==@MOMCLIENT=notconnected==" or element.text == "MOMLM=connected=LMCARDTYPELOGIN=Card Type Login@MOMCLIENT=notconnected==" or element.text == "MOMLM=connected=LMCARDTYPELOGIN=Card Type Login@MOMCLIENT=connected==":
+                    printer.connectionStatus = "Warning"
                 else:
-                    printer.connectionStatus = "Error"
+                    printer.connectionStatus = "Warning"
             elif element.attrib['name'] == "LastDataReceivedTimeStamp":
                 printer.time = int(time.time() - float(element.text))
-                if  printer.time > 7200:
+                if printer.time > printer.critTime:
                     printer.timeStatus = "Error"
-                elif printer.time > 1800:
+                elif printer.time > printer.warnTime:
                     printer.timeStatus = "Warning"
                 else:
                     printer.timeStatus = "Ok"
@@ -80,18 +89,35 @@ def get_data(connect="", user="", password="", limit=""):
 
     return printers
 
-def evaluate_data(printers=[]):
+def evaluate_data(printers=[], descriptions={}):
     for printer in printers:
-        if printer.connectionStatus == "Ok" and printer.timeStatus == "Ok":
-            message = "0 MEAP_" + printer.serialNumber + " run_age=" + str(printer.time) + ";1800;7200;; Printer " + printer.serialNumber + " is connected"
-        elif printer.connectionStatus == "Error":
-            message = "2 MEAP_" + printer.serialNumber + " run_age=" + str(printer.time) + ";1800;7200;; Printer " + printer.serialNumber + " has MEAP Application Error (!!)"
-        elif printer.timeStatus == "Error":
-            message = "1 MEAP_" + printer.serialNumber + " run_age=" + str(printer.time) + ";1800;7200;; Printer " + printer.serialNumber + " did not connect(!!) in the last " + str(printer.time/60) + " minutes"
-        elif printer.timeStatus == "Warning":
-            message = "1 MEAP_" + printer.serialNumber + " run_age=" + str(printer.time) + ";1800;7200;; Printer " + printer.serialNumber + " did not connect(!) in the last " + str(printer.time/60) + " minutes"
+        # try to resolve description
+        if printer.serialNumber in descriptions:
+            description = descriptions[printer.serialNumber]
         else:
-            message = "0 MEAP_" + printer.serialNumber + " - Unknown Printer " + printer.serialNumber + " Status"
+            description = printer.serialNumber
+            
+        if printer.connectionStatus == "Ok" and printer.timeStatus == "Ok" and printer.deviceStatus == "Ok":
+            message = "0 MEAP_" + printer.serialNumber + " run_age=" + str(printer.time) + ";" + str(printer.warnTime) + ";" + str(printer.critTime) + ";; " + description + " is connected"  
+        elif printer.deviceStatus == "Warning":
+            message = "1 MEAP_" + printer.serialNumber + " run_age=" + str(printer.time) + ";" + str(printer.warnTime) + ";" + str(printer.critTime) + ";; " + description + " (" + printer.ipAddress + ") Card Reader Error (!!)"
+        elif printer.connectionStatus == "Warning":
+            if printer.connectionDescription == "MOMLM=connected==" or printer.connectionDescription == "MOMLM=connected=LMCARDTYPELOGIN=Card Type Login":
+                message = "1 MEAP_" + printer.serialNumber + " run_age=" + str(printer.time) + ";" + str(printer.warnTime) + ";" + str(printer.critTime) + ";; " + description + " (" + printer.ipAddress + ") Secure Scan Print Application(!) not loaded"
+            elif printer.connectionDescription == "MOMLM=connected==@MOMCLIENT=notconnected==" or printer.connectionDescription == "MOMLM=connected=LMCARDTYPELOGIN=Card Type Login@MOMCLIENT=notconnected==":
+                message = "1 MEAP_" + printer.serialNumber + " run_age=" + str(printer.time) + ";" + str(printer.warnTime) + ";" + str(printer.critTime) + ";; " + description + " (" + printer.ipAddress + ") Secure Scan Print Application(!) not connected"
+            elif printer.connectionDescription == "MOMLM=connected=LMCARDTYPELOGIN=Card Type Login@MOMCLIENT=connected==":
+                message = "1 MEAP_" + printer.serialNumber + " run_age=" + str(printer.time) + ";" + str(printer.warnTime) + ";" + str(printer.critTime) + ";; " + description + " (" + printer.ipAddress + ") Secure Scan Print Application(!) configuration not loaded"
+            elif printer.connectionDescription == "MOMLM=notconnected==@MOMCLIENT=notconnected==":
+                message = "1 MEAP_" + printer.serialNumber + " run_age=" + str(printer.time) + ";" + str(printer.warnTime) + ";" + str(printer.critTime) + ";; " + description + " (" + printer.ipAddress + ") Universal Login Manager(!) not connected"
+            else:
+                message = "1 MEAP_" + printer.serialNumber + " run_age=" + str(printer.time) + ";" + str(printer.warnTime) + ";" + str(printer.critTime) + ";; " + description + " (" + printer.ipAddress + ") Universal Login Manager(!) not loaded"
+        elif printer.time > printer.critTime:
+            message = "1 MEAP_" + printer.serialNumber + " run_age=" + str(printer.time) + ";" + str(printer.warnTime) + ";" + str(printer.critTime) + ";; " + description + " (" + printer.ipAddress + ") did not connect since " + (str(int(printer.time/3600/24)) + " days(!)" if printer.time/3600 >= 24 else str(int(printer.time/3600)) + " hours(!)")
+        elif printer.time > printer.warnTime:
+            message = "1 MEAP_" + printer.serialNumber + " run_age=" + str(printer.time) + ";" + str(printer.warnTime) + ";" + str(printer.critTime) + ";; " + description + " (" + printer.ipAddress + ") did not connect since " + str(int(printer.time/3600)) + " hours " + str(int((printer.time%3600/60))) + " minutes"
+        else:
+            message = "3 MEAP_" + printer.serialNumber + " - " + description + " (" + printer.ipAddress + ") unknown status"
         print(message)
 
 def main(argv=None):
@@ -121,15 +147,25 @@ def main(argv=None):
                             action='store',
                             default='15')
 
+    parser.add_argument('-d', '--descriptions', dest='descriptions',
+                        help='printer description/names json filename to translate from serial numbers',
+                        action='store')
+
     try:
         args = parser.parse_args()
     except:
         # Something didn't work. We will return an unknown.
         print(' invalid argument(s) {usage}'.format(usage=parser.format_usage()))
         sys.exit(3)
+    
+    descriptions = {}
+
+    if args.descriptions:
+        with open(args.descriptions, 'r') as file:
+            descriptions = json.load(file)
 
     print("<<<local>>>")
-    evaluate_data(printers=get_data(connect=args.connect, user=args.user, password=args.password, limit=args.limit))
+    evaluate_data(printers=get_data(connect=args.connect, user=args.user, password=args.password, limit=args.limit), descriptions=descriptions)
 
 
 if __name__ == '__main__':
